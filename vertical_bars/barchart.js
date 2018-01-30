ba884aa6aa1d322b68fed6e9e57081559c734b3d
Thomas Clarke <TClarke@scottlogic.com>
2018-01-30 14:57:56 +0000

    New data source and data domain

--- vertical_bars/barchart.js
+++ vertical_bars/barchart.js
@@ -2,7 +2,9 @@ console.info("Downloaded started...");
 
 let width = 960;
 let height = 500;
-let bar_height = 20;
+
+var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .1);
 
 let y = d3.scale.linear()
         .range([height, 0]);
@@ -12,40 +14,37 @@ let chart = d3.select(".chart")
     .attr("height", height)
     
 
-d3.tsv("http://localhost:3100/data/valoos.tsv", type, function(error, duto){
+d3.tsv("http://localhost:3100/data/alphabet.tsv", type, function(error, duto){
     console.info("Downloaded!");
     console.info(duto);
 
-    let val = d3.max(duto, function(d) { return d.value; });
+    x.domain(duto.map(function(d) { return d.letter; }));
+    let val = d3.max(duto, function(d) { return d.frequency; });
     console.info(val);
     y.domain([0,val])
-
-    let bar_width = width / duto.length;
 
     let bar = chart.selectAll("g")
                     .data(duto)
                     .enter().append("g")
                     .attr("transform", function(d, i) { 
-                        console.info("output:", i*bar_width);
-                        return "translate("+ (i * bar_width) + ",0)"; });
+                        return "translate("+ x(d.letter) + ",0)"; });
 
     bar.append("rect")
-        .attr("y", function(d){ return y(d.value); })
-        .attr("height", function(d) { 
-            return height - y(d.value); } )
-        .attr("width", bar_width - 1);
+        .attr("y", function(d){ return y(d.frequency); })
+        .attr("height", function(d) { return height - y(d.frequency); } )
+        .attr("width", x.rangeBand());
     
         
     bar.append("text")
-        .attr("x", bar_width / 2 )
-        .attr("y", function(d){ return y(d.value) + 3; })
+        .attr("x", x.rangeBand() / 2 )
+        .attr("y", function(d){ return y(d.frequency) + 3; })
         .attr("dy", ".75em")
-        .text(function(d) { return d.name; });
+        .text(function(d) { return d.letter; });
 });
 
 console.info("Downloading...");
 
 function type(d) {
-    d.value = parseInt(d.value); // coerce to number
+    d.frequency = +d.frequency; // coerce to number
     return d;
 }
