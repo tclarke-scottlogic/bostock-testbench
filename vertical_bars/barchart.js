9f50a1c8a4826b8b4d56b96280929c21cdb57c08
Thomas Clarke <TClarke@scottlogic.com>
2018-01-30 15:50:54 +0000

    Axes

--- vertical_bars/barchart.js
+++ vertical_bars/barchart.js
@@ -1,45 +1,64 @@
 console.info("Downloaded started...");
 
-let width = 960;
-let height = 500;
+var margin = { top: 20, bottom: 30, left: 40, right: 30 },
+    width = 800 - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom;
 
 var x = d3.scale.ordinal()
     .rangeRoundBands([0, width], .1);
 
 let y = d3.scale.linear()
-        .range([height, 0]);
+    .range([height, 0]);
+
+let x_axis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+
+var y_axis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .ticks(10, "%");
 
 let chart = d3.select(".chart")
-    .attr("width", width)
-    .attr("height", height)
-    
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.tsv("http://localhost:3100/data/alphabet.tsv", type, function(error, duto){
+d3.tsv("http://localhost:3100/data/alphabet.tsv", type, function (error, duto) {
     console.info("Downloaded!");
     console.info(duto);
 
-    x.domain(duto.map(function(d) { return d.letter; }));
-    let val = d3.max(duto, function(d) { return d.frequency; });
+    x.domain(duto.map(function (d) { return d.letter; }));
+    let val = d3.max(duto, function (d) { return d.frequency; });
     console.info(val);
-    y.domain([0,val])
+    y.domain([0, val])
 
-    let bar = chart.selectAll("g")
-                    .data(duto)
-                    .enter().append("g")
-                    .attr("transform", function(d, i) { 
-                        return "translate("+ x(d.letter) + ",0)"; });
+    chart.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(x_axis);
 
-    bar.append("rect")
-        .attr("y", function(d){ return y(d.frequency); })
-        .attr("height", function(d) { return height - y(d.frequency); } )
+    chart.append("g")
+        .attr("class", "y axis")
+        .call(y_axis)
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Frequency");
+
+    chart.selectAll(".bar")
+        .data(duto)
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function (d) { return x(d.letter); })
+        .attr("y", function (d) { return y(d.frequency); })
+        .attr("height", function (d) { return height - y(d.frequency); })
         .attr("width", x.rangeBand());
-    
-        
-    bar.append("text")
-        .attr("x", x.rangeBand() / 2 )
-        .attr("y", function(d){ return y(d.frequency) + 3; })
-        .attr("dy", ".75em")
-        .text(function(d) { return d.letter; });
+
 });
 
 console.info("Downloading...");
